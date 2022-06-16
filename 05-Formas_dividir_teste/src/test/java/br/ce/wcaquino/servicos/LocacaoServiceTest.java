8ac2bb12eb7e59a4ca4c0e52b8c0d8e107159160
Wendel CIGA <wfrsilva@gmail.com>
2022-06-16 00:04:04 -0300

    Formas de dividir um teste
    https://www.cod3r.com.br/courses/take/testes-unitarios-em-java/lessons/9516756-formas-de-dividir-um-teste
    
    error.checkThat pode exibir todos os erros, metodo anterior só mostrava o primeiro erro.

--- 05-Formas_dividir_teste/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ 05-Formas_dividir_teste/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -2,13 +2,16 @@ package br.ce.wcaquino.servicos;
 
 
 import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 import java.util.Date;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -16,6 +19,10 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
 	public void testeLocacao() {
 		//cenario
@@ -29,14 +36,14 @@ public class LocacaoServiceTest {
 		//verificacao
 		//System.out.println(locacao.getValor());
 		
-		assertThat(locacao.getValor(), is(5.0));
-		assertThat(locacao.getValor(), is(not(6.0)));
+		error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+		error.checkThat(locacao.getValor(), is(not(6.0)));
 		
 		//System.out.println(locacao.getDataLocacao());
-		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
 		
 		//System.out.println(locacao.getDataRetorno());
-		assertThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(false));
 		
 	}//teste
 
